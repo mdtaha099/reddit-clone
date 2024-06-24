b8ccef1eb5cac2c0d61a305dc1c339d3d3d592f6
somesh <somesh.thakur.28.2@mountblue.tech>
2024-06-24 11:29:42 +0530

    modify subreddit service to fetch only posts

--- src/main/java/io/mountblue/reddit_clone/dao/SubredditRepository.java
+++ src/main/java/io/mountblue/reddit_clone/dao/SubredditRepository.java
@@ -3,8 +3,14 @@ package io.mountblue.reddit_clone.dao;
 import io.mountblue.reddit_clone.entity.Post;
 import io.mountblue.reddit_clone.entity.Subreddit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface SubredditRepository extends JpaRepository<Subreddit,Integer> {
+    @Query("FROM Post p " +
+            "INNER JOIN p.subreddit s " +
+            "WHERE p.isPost = true " +
+            "AND p.subreddit.id = :id")
+    List<Post> findAllPostsById(int id);
 }
