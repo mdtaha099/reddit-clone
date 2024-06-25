ab6d7326bcebf0765dcfd3618e4ada272f3b71bd
Kamal Mohanan <kamal.nair.28.2@mountblue.tech>
2024-06-25 11:28:29 +0530

    Basic authentication

---
+++ src/main/java/io/mountblue/reddit_clone/security/SecurityConfig.java
@@ -0,0 +1,41 @@
+package io.mountblue.reddit_clone.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.setUsersByUsernameQuery(
+                "select username, password, is_active from user where username=?");
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
+                "select username, authority from user where username=?"
+        );
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(configurer ->
+                        configurer
+                                .anyRequest().permitAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(LogoutConfigurer::permitAll)
+                .csrf(AbstractHttpConfigurer::disable)
+        ;
+
+        return httpSecurity.build();
+    }
+}
